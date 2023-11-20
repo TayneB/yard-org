c7a74f3a08b225bbe130097b7257f1f9fd8ffe1c
Tayne Bilyard <taynebilyard@gmail.com>
2023-11-20 22:55:06 +1300

    getAllEvents db function and / endpoint implemented, api converts from snake to camel

--- server/db/events.ts
+++ server/db/events.ts
@@ -1,6 +1,15 @@
 import db from './connection.ts'
 import { Event } from '../../models/event.ts'
 
-export async function getAllFruits(): Promise<Event[]> {
-  return db('events').select()
+export async function getAllEvents(): Promise<Event[]> {
+  return db('events').select([
+    'id',
+    'name',
+    'event_date as eventDate',
+    'description',
+    'attendees',
+    'created_by as createdBy',
+    'date_created as dateCreated',
+    'edit_note as editNote',
+  ])
 }
